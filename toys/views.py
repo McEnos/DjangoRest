9e0797409cf2d0c1ec676ad46a9b2f40124aa4c2
Joshua <joshua.odiwuor@turnkeyafrica.com>
2020-05-08 08:11:55 +0300

    Adds django rest generic Response class instead of JSONResponse.

--- toys/views.py
+++ toys/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
 from toys.models import Toy
 from toys.serializers import ToySerializer
@@ -15,23 +15,22 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def toy_list(request):
     if request.method == 'GET':
         toys = Toy.objects.all()
         toys_serializer = ToySerializer(toys, many=True)
-        return JSONResponse(toys_serializer.data)
+        return Response(toys_serializer.data)
 
     elif request.method == 'POST':
-        toy_data = JSONParser().parse(request)
-        toy_serializer = ToySerializer(data=toy_data)
+        toy_serializer = ToySerializer(data=request.data)
         if toy_serializer.is_valid():
             toy_serializer.save()
-            return JSONResponse(toy_serializer.data, status=status.HTTP_201_CREATED)
-        return JSONResponse(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(toy_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def toy_detail(request, pk):
     try:
         toy = Toy.objects.get(pk=pk)
@@ -40,15 +39,14 @@ def toy_detail(request, pk):
 
     if request.method == 'GET':
         toy_serializer = ToySerializer(toy)
-        return JSONResponse(toy_serializer.data)
+        return Response(toy_serializer.data)
 
     elif request.method == 'PUT':
-        toy_data = JSONParser().parse(request)
-        toy_serializer = ToySerializer(toy, data=toy_data)
+        toy_serializer = ToySerializer(toy, data=request.data)
         if toy_serializer.is_valid():
             toy_serializer.save()
-            return JSONResponse(toy_serializer.data)
-        return JSONResponse(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(toy_serializer.data)
+        return Response(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'DELETE':
         toy.delete()
